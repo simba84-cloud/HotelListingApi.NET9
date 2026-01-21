1269a0ae3726182d2a40723c2d58e998457105ab
SimbarasheMuzika <SimbarasheMuzika@SIMBARASHE>
2026-01-21 15:37:56 +0200

    Add project files.

---
+++ HotelListingApi/Program.cs
@@ -0,0 +1,32 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyHeader()
+              .AllowAnyOrigin()
+              .AllowAnyMethod());
+});
+builder.Host.UseSerilog((ctx,lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseSerilogRequestLogging(); // logs get ,post endpoints 
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

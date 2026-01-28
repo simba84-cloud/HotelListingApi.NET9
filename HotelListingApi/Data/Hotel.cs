91d15a7a9317228b6c614792077d896cccac9219
Simbarashe Muzika <simbamuzika@gmail.com>
2026-01-28 06:16:09 +0200

    added migration

---
+++ HotelListingApi/Data/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingApi.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }// foreign key
+        public Country Country { get; set; }
+    }
+
+   
+}
--- HotelListingApi/Data/HotelListingDbContext.cs
+++ HotelListingApi/Data/HotelListingDbContext.cs
@@ -8,5 +8,48 @@ namespace HotelListingApi.Data
         {
             
         }
+        public DbSet<Hotel>Hotels { get; set; }
+        public DbSet<Country>Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country 
+                { 
+                Id= 1,
+                Name="zimbabwe",
+                ShortName = "ZW"
+                },
+                 new Country
+                 {
+                     Id = 2,
+                     Name = "south",
+                     ShortName = "SA"
+                 }
+
+                );
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "zaniu",
+                    Address = "123 amisi",
+                    CountryId = 121,
+                    Rating = 3
+
+                },
+                 new Hotel
+                 {
+                     Id = 1,
+                     Name = "abwe",
+                     Address = "1zanui",
+                     CountryId = 121,
+                     Rating = 0
+                 }
+
+                );
+        }
     }
 }
